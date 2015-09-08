3239ddb15a973d31128e05e5fc322449c490d762
apcel <apcel@yandex.ru>
2015-09-09 00:12:53 +0300

    Правлю оформление.

--- prog/2014/ivb-3-14/Максимов А.Ю/3. Курсовая -_-/main.cpp
+++ prog/2014/ivb-3-14/Максимов А.Ю/3. Курсовая -_-/main.cpp
@@ -186,9 +186,13 @@ int main (int argc, char* argv[])
     while(server_reply.find("\r\n\r\n") == server_reply.npos) {
         recv(socketFd, server_reply_buf, sizeof(char), 0);
         server_reply.push_back(*server_reply_buf);
-
     }
+
     log(server_reply);
+    if(server_reply.find('\0') != server_reply.npos) {
+        log(stderr, "Ughm, could parse response header illegally, sry.");
+    }
+
     server_reply.push_back('\0');
     for(int i = 0; server_reply[i] != '\0'; ++i)
         server_reply[i] = std::toupper(server_reply[i]);
@@ -246,42 +250,44 @@ int gethost(char* address, linkStruct * result) {
         temp = 0;
         result->protocol = "http";
     }
+
     temp = s.find("/", begin + 1);
-    if (temp != s.npos)
-    {
+    if (temp != s.npos) {
         end = temp;
         result->hostname = s.substr(begin, abs(end - begin));
         result->relative = s.substr(end);
     }
+
     if(result->filename == "") {
         temp = s.rfind("/");
         end = s.find("?", temp) - 1;
         if(temp != s.npos) {
             if(end == s.npos - 1)
                 result->filename = s.substr(temp + 1);
+
             result->filename = s.substr(temp + 1, abs(end - temp));
         } else {
             return -2;
         }
     }
 
-#    ifdef DEBUG_GETHOST
-    log("gethost returns:");
-    log("begin = " + std::to_string(begin));
-    log("end   = " + std::to_string(end));
-    //log("ukhm, host may be the \'" + s + "\'");
-    log("result->protocol = " + result->protocol);
-    log("result->hostname = " + result->hostname);
-    log("result->relative = " + result->relative);
-    log("result->filename = " + result->filename);
-#    endif
+#   ifdef DEBUG_GETHOST
+        log("gethost returns:");
+        log("begin = " + std::to_string(begin));
+        log("end   = " + std::to_string(end));
+        //log("ukhm, host may be the \'" + s + "\'");
+        log("result->protocol = " + result->protocol);
+        log("result->hostname = " + result->hostname);
+        log("result->relative = " + result->relative);
+        log("result->filename = " + result->filename);
+#   endif
     return 0;
 }
 
 void log(std::string  message) {
-#    ifdef DEBUG
-    log(stdout, message);
-#    endif
+#   ifdef DEBUG
+        log(stdout, message);
+#   endif
 }
 void log( FILE * fd, std::string message) {
     fprintf(fd, "%s\n", message.c_str());
